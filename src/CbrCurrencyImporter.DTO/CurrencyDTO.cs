d2af2275e3600065403a8c4f236061d73ab498e7
rds044 <rds044@DESKTOP-59UFLJ1>
2025-02-27 22:35:54 +0300

    feat: Добавлена DTO модель, дополнена структура проекта

---
+++ src/CbrCurrencyImporter.DTO/CurrencyDTO.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CbrCurrencyImporter.Api
+{
+    /// <summary>
+    /// DTO модель курсов валют, ЦБ всегда выводит все курсы валют, поэтому nullable не использую
+    /// </summary>
+    public class CurrencyDTO
+    {
+        /// <summary>
+        /// Код курса валют
+        /// </summary>
+        public string CurrencyCode { get; set; }
+
+        /// <summary>
+        /// Сокращенное имя валюты
+        /// </summary>
+        public string CurrencyName { get; set; }
+        /// <summary>
+        /// Стоимость в рублях 1 шт иностранной валюты  
+        /// </summary>
+        public decimal Rate { get; set; }
+
+        /// <summary>
+        /// Количество шт иностранной валюты приравненно к Value 
+        /// </summary>
+        public decimal Value { get; set; }
+        /// <summary>
+        /// Количество шт иностранной валюты приравненно к Value 
+        /// </summary> 
+        public int Nominal { get; set; }
+        /// <summary>
+        /// Время и дата сбора данных
+        /// </summary>
+        public DateTime Date { get; set; }
+    }
+}
